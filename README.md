023c2f091f2ace5455c0d7d9f77e70fe113e1392
Alyssa Concepcion <alyssanconcepcion@gmail.com>
2021-05-14 23:32:52 -0700

    Added video demo and finished readME

--- README.md
+++ README.md
@@ -1,14 +1,43 @@
-## Team Profile Generator
-This is a node.js based tool to produce a webpage for any team. All the user must do is 
-answer a questionnaire regarding themselves and their team details including the manager,
-engineers, and interns. The result is a clean and functional page that showcases
-profiles of each team member.
+# Team Profile Generator
+MIT[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
+## Table of Contents 
+- [Description](#description)
+- [Demonstration](#demonstration)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
 
-# Technologies Used:
-HTML, CSS, JavaScript, Node.js, VS Code
 
-# License
-MIT
+## Description 
+This is a node.js based tool to produce a webpage for any team. All the user must do is answer a questionnaire regarding themselves and their team details including the manager,engineers, and interns. The result is a clean and functional page that showcases profiles of each team member.
 
-# Demonstration:
-(video link here)
+## Demonstration:
+Click here to view a demo (https://drive.google.com/file/d/106VrHJEmMih6AX0PTHi2zwrLrZVFgs9J/view?usp=sharing)
+
+## Installation
+To install necessary dependencies, run the following command:
+
+`npm i inquirer
+npm i jest
+`
+
+## Usage
+Use for any team looking to build a fast and easy profile
+    
+## Contributing
+Please email me for any suggestions.
+
+## Tests
+To run tests, run the following command:
+`
+npm test
+`
+
+# Questions
+Hello my name is Alyssa Concepcion, you can reach me at alyssanconcepcion@gmail.com
+My GitHub username is hackerlyss
+Live link to my repository: [github repository](https://github.com/hackerlyss/TeamProfileGenerator)
+
+
--- index.js
+++ index.js
@@ -1,176 +1,191 @@
+// Dependencies up here
 const fs = require("fs");
 const inquirer = require("inquirer");
-const Manager = require("./lib/Manager.js");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
-const profilePage = require("./src/profilePage.js");
+// Subclasses here
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 const officeStaff = [];
 
-function writeProfile() {
-    const filename = `${response.team.split(' ').join('')}Readme.md`;
-    fs.writeFile(filename, readMe(profilePage), (err) =>
-    err ? error.log(err) : console.log("success"));
+// The function to home all functions
+function generateProfile() {
+    startHtml();
+    addStaff();
 }
 
-function addEngineer() {
-    inquirer
-    .prompt([
+function addStaff() {
+    inquirer.prompt([
         {
-            type: 'input',
-            message: 'Please enter your name.',
-            name: 'name'
+            type:"input",
+            message:"Enter team member's name.",
+            name:"name"
         },
         {
-            type: 'input',
-            message: 'Please enter your employee ID.',
-            name: 'id'
-        },
-        {
-            type: 'input',
-            message: 'Please enter your email.',
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: 'Please enter your GitHub username.',
-            name: 'github'
-        },
-        {
-            type: 'checkbox',
-            message: 'Please add another team member:',
-            name: 'team',
+            type:"list",
+            message:"Select team member's role:",
             choices: [
-                {
-                    name: "Engineer"
-                },
-                {
-                    name: "Intern"
-                },
-                {
-                    name: "I am finished with adding team members"
-                }
+                "Manager",
+                "Engineer",
+                "Intern"
             ],
-        },
-    ]).then(response => {
-    const engineer = {"name": response.name, "ID": response.id, "email": response.email, "phone": response.phone, "github": response.github, "role": "engineer" };
-    officeStaff.push(engineer);
-    if(response.team === "Engineer") {
-        addEngineer();
-    }
-    if(response.team === "Intern") {
-        addIntern();
-    }else {
-        writeProfile()
-    }})
-}
-module.export = addEngineer()
-
-function addIntern() {
-    inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Please enter your name.',
-            name: 'name'
+            name: "role"
         },
         {
-            type: 'input',
-            message: 'Please enter your employee ID.',
-            name: 'id'
+            type:"input",
+            message:"Enter team member's ID",
+            name:"id"
         },
         {
-            type: 'input',
-            message: 'Please enter your email.',
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: 'Please enter the name of the school currently attending.',
-            name: 'school'
-        },
-        {
-            type: 'checkbox',
-            message: 'Please add another team member:',
-            name: 'team',
-            choices: [
-                {
-                    name: "Engineer"
-                },
-                {
-                    name: "Intern"
-                },
-                {
-                    name: "I am finished with adding team members"
-                }
-            ],
-        },
-    ]).then(response => {
-    const intern = {"name": response.name, "ID": response.id, "email": response.email, "phone": response.phone, "school": response.school, "role": "intern" };
-    officeStaff.push(intern);
-    if(response.team === "Engineer") {
-        addEngineer();
-    }
-    if(response.team === "Intern") {
-        addIntern();
-    }else {
-        writeProfile()
-    }})
-}
-
-inquirer
-.prompt([
-    {
-        type: 'input',
-        message: 'What is the name of your team?',
-        name: 'team'
-    },
-    {
-        type: 'input',
-        message: 'What is the name of your manager?',
-        name: 'name'
-    },
-    {
-        type: 'input',
-        message: 'What is the your employee ID #?',
-        name: 'id'
-    },
-    {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email'
-    },
-    {
-        type: 'input',
-        message: 'What is your office phone number?',
-        name: 'phone'
-    },
-    {
-        type: 'checkbox',
-        message: 'Please add another team member:',
-        name: 'team',
-        choices: [
+            type:"input",
+            message:"Enter team member's email",
+            name:"email"
+        }  
+    ]).then(function({name,role,id,email}) {
+        let roleInfo = "";
+        if (role === "Engineer") {
+            roleInfo = "GitHub username";
+        }else if (role === "Intern") {
+            roleInfo = "school name";
+        }else {
+            roleInfo = "office phone number";
+        }
+        inquirer.prompt([
             {
-                name: "Engineer"
+                type:"input",
+                message:`Enter team member's ${roleInfo}`,
+                name: "roleInfo"
             },
             {
-                name: "Intern"
-            },
-            {
-                name: "I am finished with adding team members"
+                type:"list",
+                message:"Would you like to add more team profiles?",
+                choices: [
+                    "yes",
+                    "no"
+                ],
+                name:"moreStaff"
             }
-        ],
+        ]).then(function({roleInfo, moreStaff}) {
+            let newStaff;
+            if (role === "Engineer") {
+                newStaff = new Engineer(name,id,email,roleInfo);
+
+            }else if (role === "Intern") {
+                newStaff = new Intern(name,id,email,roleInfo);
+            }else {
+                newStaff = new Manager(name,id,email,roleInfo);
+            }
+            officeStaff.push(newStaff);
+            addHtml(newStaff)
+            .then(function() {
+                if (moreStaff === "yes") {
+                    addStaff();
+                }else {
+                    finishHtml();
+                }
+            })
+        })
+    })
+}
+
+function startHtml() {
+    const html = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+</head>
+<body>
+<div class="container">
+        <h1 class="display-1 ">Team Profile</h1>
+        <div class="row">
+        
+`;
+fs.writeFile("./output/team.html",html, function(err) {
+    if(err) {
+        console.log(err);
+    
     }
-]).then(response => {
-    const manager = {"name": response.name, "ID": response.id, "email": response.email, "phone": response.phone, "role": "manager" };
-    officeStaff.push(manager);
-    if(response.team === "Engineer") {
-        addEngineer();
-    }
-    if(response.team === "Intern") {
-        addIntern();
-    }else {
-        writeProfile();
-            
-    }
-    }
-       )
+})
+}
+
+function addHtml(staff) {
+    return new Promise(function(resolve,reject) {
+        const name = staff.getName();
+        const role = staff.getRole();
+        const id = staff.getId();
+        const email = staff.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const github = staff.getGithub();
+            data = `
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+              <ul class = "list-group list -group-flush">
+              <li class="list-group-item">ID: ${id}</li>
+              <li class="list-group-item">Email: ${email}</li>
+              <li class="list-group-item">GitHub: ${github}</li>
+              </ul>
+            </div>
+          </div>
+            `
+        }else if (role === "Intern") {
+            const school = staff.getSchool();
+            data = `
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+              <ul class = "list-group list -group-flush">
+              <li class="list-group-item">ID: ${id}</li>
+              <li class="list-group-item">Email: ${email}</li>
+              <li class="list-group-item">School: ${school}</li>
+              </ul>
+            </div>
+          </div>`
+        }else {
+            const phoneNumber = staff.getPhone();
+            data = `
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${name}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+              <ul class = "list-group list -group-flush">
+              <li class="list-group-item">ID: ${id}</li>
+              <li class="list-group-item">Email: ${email}</li>
+              <li class="list-group-item">Office Phone: ${phoneNumber}</li>
+              </ul>
+            </div>
+          </div>`
+        }
+        fs.appendFile("./output/team.html",data,function(err) {
+            if(err) {
+                return reject(err);
+            };
+            return resolve();
+        })
+    })
+}
+
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    </body>
+    </html>`
+    fs.appendFile("./output/team.html",html,function(err) {
+        if(err) {
+            console.log(err);
+        }
+    });
+     console.log("Team profile complete")
+}
+generateProfile();
